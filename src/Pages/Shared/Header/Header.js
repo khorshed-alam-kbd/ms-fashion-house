5ac48e8fcbecbef71b5a02ac239d19849e69c7f9
unknown <khorshedalam.kbd@gmail.com>
2023-05-14 10:31:47 +0600

    upgradation

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../Assets/Images/ms-fashion.png'
+import userlogo from '../../../Assets/Images/user-images.png'
 
 const Header = () => {
 
@@ -42,7 +43,7 @@ const Header = () => {
                 <div className="form-control hidden lg:flex">
                     <input type="text" placeholder="Search" className="input input-bordered" />
                 </div>
-                {/* <div className="dropdown dropdown-end hidden lg:flex">
+                <div className="dropdown dropdown-end hidden lg:flex">
                     <label tabIndex={0} className="btn btn-ghost btn-circle">
                         <div className="indicator">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
@@ -62,7 +63,7 @@ const Header = () => {
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
-                            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            <img src={userlogo}/>
                         </div>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
@@ -75,7 +76,7 @@ const Header = () => {
                         <li><a>Settings</a></li>
                         <li><a>Logout</a></li>
                     </ul>
-                </div> */}
+                </div>
             </div>
         </div>
     );
